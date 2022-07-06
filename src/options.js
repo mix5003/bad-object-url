90feef339a151eabd3badacba9e23d5dd1bff1c1
Onurtag <Onurtag@users.noreply.github.com>
2022-07-06 19:41:49 +0300

    bigger test file

--- src/options.js
+++ src/options.js
@@ -2,11 +2,18 @@ function downloadBlob(event) {
   event.preventDefault();
   event.stopPropagation();
 
-  let data = JSON.stringify([1,2,3,4,5]);
-  let blob = new window.Blob([ data ]);
+  //Creates a 15728640 byte file
+  let blobs = [];
+  for (let index = 0; index < 20; index++) {
+    let blob = new Blob([index.toString().repeat(1024 * 512)]);
+    blobs.push(blob);
+  }
+  
+  let thefile = new File(blobs, "file");
+
+  let objectUrl = URL.createObjectURL(thefile);
 
   let a = document.createElement('a');
-  let objectUrl = window.URL.createObjectURL(blob);
   a.href = objectUrl;
   a.download = 'data.json';
   a.target = '_blank';
